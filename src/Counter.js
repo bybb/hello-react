00bf0b7aa040ce0eee27392a85d6cd79ade54abf
pkyy <ai9gle@gmail.com>
2021-03-31 21:04:07 +0900

    event#1

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        // state의 초깃값 설정하기
-        this.state = {
-            number : 0,
-            fixedNumber : 0
-        };
+    state = {
+        number : 0,
+        fixedNumber : 0
     }
     render() {
         const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
@@ -19,7 +15,15 @@ class Counter extends Component {
                     // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다.
                     onClick={()=>{
                         // this.state를 사용하여 새로운 값을 넣을 수 있다.
-                        this.setState({number:number+1});
+                        this.setState(
+                            {
+                                number : number + 1
+                            },
+                            () => {
+                                console.log('방금 setState가 호출되었습니다.');
+                                console.log(this.state);
+                            }
+                        )
                     }}
                 >
                     +1
